956132ed6a9351fd0cfc451d119f6fb2dd43e0e6
shinji ichien <shinji11088011@gmail.com>
2021-01-16 18:54:29 +0900

    残存距離

--- pages/hint/[id].tsx
+++ pages/hint/[id].tsx
@@ -3,11 +3,25 @@ import { useGeolocation } from "react-use";
 import { Button, Container, Row } from "react-bootstrap";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useMemo } from "react";
+import { distm, Point } from "../../services/geo";
+
+const goalPoint = { lat: 36.7018268, lng: 137.2126258 };
 
 const Hint = () => {
   const state = useGeolocation();
   const router = useRouter();
   const { id } = router.query;
+
+  const dist = useMemo(() => {
+    const { latitude, longitude } = state;
+
+    const from =
+      latitude && longitude ? { lat: latitude, lng: longitude } : null;
+
+    const dist = distm(from, goalPoint);
+    return dist;
+  }, [state]);
 
   return (
     <Container>
@@ -17,6 +31,7 @@ const Hint = () => {
       <Row>
         <h2>lat: {state.latitude}</h2>
         <h2>lon: {state.longitude}</h2>
+        <h2>距離: {dist}m</h2>
       </Row>
       <Row>
         <Link href="/good">
--- services/geo.ts
+++ services/geo.ts
@@ -8,6 +8,12 @@ export interface Point {
 /**
  * @see https://turfjs.org/docs/#distance
  */
-export const distKm = (from: Point, to: Point) => {
-  return distance([from.lng, from.lat], [to.lng, to.lat]);
+export const distm = (from: Point | null, to: Point | null): number | null => {
+  if (from === null || to === null) {
+    return null;
+  }
+
+  const distKm = distance([from.lng, from.lat], [to.lng, to.lat]);
+  const distMeter = distKm * 1000;
+  return Math.round(distMeter);
 };
