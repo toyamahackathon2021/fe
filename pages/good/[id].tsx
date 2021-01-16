934dbaf129c67a96af64eaf1a4a3db03b22b2099
penxera <kool.arakaze@gmail.com>
2021-01-17 01:57:04 +0900

    good

--- pages/good/[id].tsx
+++ pages/good/[id].tsx
@@ -1,14 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Button, Figure } from "react-bootstrap";
+import { Container, Col, Row, Button, Image } from "react-bootstrap";
 
 const style = `
+* {
+  text-align: center;
+}
 .container {
-  // background: url("/bg.png") no-repeat;
-  background: #fff;
-  margin: 20px;
+  background-color: #fff;
+  padding: 10px;
+ }
+ .wrapper {
+  background-image: url("/bg.png");
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 20px;
  }
  .title {
    font-size: 20px;
+ }
+ .center {
+   text-align: center;
+ }
+ .thumnail {
+   margin: 0 auto;
+ }
+ p {
+   text-align: left;
  }
 `
 
@@ -16,36 +32,43 @@ const Good = () => {
   return (
     <>
       <style>{style}</style>
-      <Container className="container">
-        <Row>
-          <h1 className="title">おめでとう！</h1>
+      <div className="wrapper">
+      <Container>
+        <Row className="">
+          <Col md="auto" className="center">
+            <h1 className="title">おめでとう！</h1>
+          </Col>
         </Row>
         <Row>
-        <Figure>
-          <Figure.Image
+          <Image
             width={90}
             height={90}
             alt="good"
             src="/kaizoku_takara.png"
+            className="thumnail"
           />
-        </Figure>
         </Row>
         <Row>
-          <p>富山城にたどり着いたね！</p>
+          <Col md="auto" className="center">
+            <h4 className="title">富山城にたどり着いたね！</h4>
+          </Col>
         </Row>
         <Row>
-          <p>
-          今いるところは、富山城址公園の一角に建つ富山城。実はこの富山城、 戦国時代に築城されました。そう、現在にいたるまで、400年以上にわたる歴史があるのです。<br />
-          <br />
-          今見ている富山城の天守閣は、残念ながら戦災後(1954年)に建設されたものです。<br />
-          <br />
-          築城されてから現在に至るまでの歴史は、今あなたがいるところの横にある富山市郷土博物館で、模型や動画などで見ることができるよ。<br />
-          <br />
-          せっかく来たのだからよかったら立ち寄ってみてね。
-          </p>
+          <Col md="auto" className="center">
+            <p>
+            今いるところは、富山城址公園の一角に建つ富山城。実はこの富山城、 戦国時代に築城されました。そう、現在にいたるまで、400年以上にわたる歴史があるのです。<br />
+            <br />
+            今見ている富山城の天守閣は、残念ながら戦災後(1954年)に建設されたものです。<br />
+            <br />
+            築城されてから現在に至るまでの歴史は、今あなたがいるところの横にある富山市郷土博物館で、模型や動画などで見ることができるよ。<br />
+            <br />
+            せっかく来たのだからよかったら立ち寄ってみてね。
+            </p>
+          </Col>
         </Row>
         <Button variant="outline-primary">戻る</Button>
       </Container>
+      </div>
     </>
   );
 };
