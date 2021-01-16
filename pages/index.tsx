771ad10314604c6bdf032aa5c68d62218683a62e
shinji ichien <shinji11088011@gmail.com>
2021-01-16 16:43:14 +0900

    geolocation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import {useGeolocation} from 'react-use'
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const state = useGeolocation();
+
+  return (
   <>
     <h1>ブリ キャッシング</h1>
       <Link href="/main">
@@ -9,7 +13,14 @@ const IndexPage = () => (
       <Link href="/goal">
         <a>ブリ goal</a>
       </Link>
+      <h2>
+        lat: {state.latitude}
+      </h2>
+      <h2>
+        lon: {state.longitude}
+      </h2>
   </>
-)
+  )
+}
 
 export default IndexPage
