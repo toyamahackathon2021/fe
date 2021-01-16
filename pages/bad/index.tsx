49c334d45e18879f421ef6e5bdbb1700efafde10
penxera <kool.arakaze@gmail.com>
2021-01-17 02:05:54 +0900

    bad

--- pages/bad/index.tsx
+++ pages/bad/index.tsx
@@ -1,30 +1,62 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Button, Figure } from "react-bootstrap";
+import { Container, Col, Row, Button, Image } from "react-bootstrap";
+
+const style = `
+* {
+  text-align: center;
+}
+.container {
+  background-color: #fff;
+  padding: 10px;
+ }
+ .wrapper {
+  background-image: url("/bg.png");
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 20px;
+ }
+ .title {
+  font-size: 20px;
+ }
+`
 
 const Bad = () => {
   return (
-    <Container>
-      <Row>
-        <h1>残念！</h1>
-      </Row>
-      <Figure>
-        <Figure.Image
-          width={90}
-          height={90}
-          alt="bad"
-          src="/kaizoku_mark.png"
-        />
-      </Figure>
-      <Row>
-        <h1>写真をもう一度確認してみよう！</h1>
-      </Row>
-      <Row>
-        <Button variant="warning">もう一度送る</Button>
-      </Row>
-      <Row>
-        <Button variant="outline-primary">戻る</Button>
-      </Row>
-    </Container>
+    <>
+      <style>{style}</style>
+      <div className="wrapper">
+      <Container>
+        <Row>
+          <Col md="auto" className="center">
+            <h1 className="title">残念！</h1>
+          </Col>
+        </Row>
+          <Col md="auto" className="center">
+            <Image
+              width={107}
+              height={91}
+              alt="bad"
+              src="/kaizoku_mark.png"
+            />
+          </Col>
+        <Row>
+          <Col md="auto" className="center">
+            <p>写真をもう一度確認してみよう！</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="auto" className="center">
+            <Button variant="warning">もう一度送る</Button>
+          </Col>
+        </Row>
+        <br/>
+        <Row>
+          <Col md="auto" className="center">
+            <Button variant="outline-primary">戻る</Button>
+          </Col>
+        </Row>
+      </Container>
+      </div>
+    </>
   );
 };
 
